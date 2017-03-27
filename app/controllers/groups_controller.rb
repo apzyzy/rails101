d3e47bb450d023a833767add58fc4da3fd2429b0
zhangsiyang <zhangsiyang_h@163.com>
2017-03-27 23:12:08 +0800

    implement my groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -57,7 +57,7 @@ class GroupsController < ApplicationController
   def quit
     @group = Group.find(params[:id])
     if current_user.is_member_of?(@group)
-      current_user.quit(@group)
+      current_user.quit!(@group)
       flash[:alert] = "已退出本讨论版！"
     else
       flash[:warning] = "你不是本讨论版成员"
